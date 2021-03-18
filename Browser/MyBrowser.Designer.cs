c4034beaf141ef6445a21228ffabc3a54ca54ebb
AlexandrSedelkov <a.sedelkov@list.ru>
2021-03-18 23:47:29 +0300

    Beginning

---
+++ Browser/MyBrowser.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace Browser
+{
+    partial class MyBrowser
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MyBrowser));
+            this.topButtonStrip = new System.Windows.Forms.ToolStrip();
+            this.backButton = new System.Windows.Forms.ToolStripButton();
+            this.forwardButton = new System.Windows.Forms.ToolStripButton();
+            this.refreshButton = new System.Windows.Forms.ToolStripButton();
+            this.homeButton = new System.Windows.Forms.ToolStripButton();
+            this.searchBox = new System.Windows.Forms.ToolStripTextBox();
+            this.searchButton = new System.Windows.Forms.ToolStripButton();
+            this.addButton = new System.Windows.Forms.ToolStripButton();
+            this.deleteButton = new System.Windows.Forms.ToolStripButton();
+            this.tabController = new System.Windows.Forms.TabControl();
+            this.topButtonStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // topButtonStrip
+            // 
+            this.topButtonStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.topButtonStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.backButton,
+            this.forwardButton,
+            this.refreshButton,
+            this.homeButton,
+            this.searchBox,
+            this.searchButton,
+            this.addButton,
+            this.deleteButton});
+            this.topButtonStrip.Location = new System.Drawing.Point(0, 0);
+            this.topButtonStrip.Name = "topButtonStrip";
+            this.topButtonStrip.Size = new System.Drawing.Size(1103, 27);
+            this.topButtonStrip.TabIndex = 0;
+            this.topButtonStrip.Text = "toolStrip1";
+            // 
+            // backButton
+            // 
+            this.backButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.backButton.Image = ((System.Drawing.Image)(resources.GetObject("backButton.Image")));
+            this.backButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(29, 24);
+            this.backButton.Text = "toolStripButton1";
+            this.backButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.forwardButton.Image = ((System.Drawing.Image)(resources.GetObject("forwardButton.Image")));
+            this.forwardButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(29, 24);
+            this.forwardButton.Text = "toolStripButton2";
+            this.forwardButton.Click += new System.EventHandler(this.ForwardButton_Click);
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.refreshButton.Image = ((System.Drawing.Image)(resources.GetObject("refreshButton.Image")));
+            this.refreshButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(29, 24);
+            this.refreshButton.Text = "toolStripButton3";
+            this.refreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // homeButton
+            // 
+            this.homeButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.homeButton.Image = ((System.Drawing.Image)(resources.GetObject("homeButton.Image")));
+            this.homeButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Size = new System.Drawing.Size(29, 24);
+            this.homeButton.Text = "toolStripButton4";
+            this.homeButton.Click += new System.EventHandler(this.HomeButton_Click);
+            // 
+            // searchBox
+            // 
+            this.searchBox.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(600, 27);
+            this.searchBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.SearchBox_KeyUp);
+            // 
+            // searchButton
+            // 
+            this.searchButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.searchButton.Image = ((System.Drawing.Image)(resources.GetObject("searchButton.Image")));
+            this.searchButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(29, 24);
+            this.searchButton.Text = "toolStripButton5";
+            this.searchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // addButton
+            // 
+            this.addButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.addButton.Image = ((System.Drawing.Image)(resources.GetObject("addButton.Image")));
+            this.addButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(29, 24);
+            this.addButton.Text = "toolStripButton6";
+            this.addButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.deleteButton.Image = ((System.Drawing.Image)(resources.GetObject("deleteButton.Image")));
+            this.deleteButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(29, 24);
+            this.deleteButton.Text = "toolStripButton7";
+            this.deleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // tabController
+            // 
+            this.tabController.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabController.Location = new System.Drawing.Point(0, 27);
+            this.tabController.Name = "tabController";
+            this.tabController.SelectedIndex = 0;
+            this.tabController.Size = new System.Drawing.Size(1103, 533);
+            this.tabController.TabIndex = 1;
+            // 
+            // MyBrowser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1103, 560);
+            this.Controls.Add(this.tabController);
+            this.Controls.Add(this.topButtonStrip);
+            this.Name = "MyBrowser";
+            this.Text = "MyBrowser";
+            this.Load += new System.EventHandler(this.MyBrowser_Load);
+            this.topButtonStrip.ResumeLayout(false);
+            this.topButtonStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip topButtonStrip;
+        private System.Windows.Forms.ToolStripButton backButton;
+        private System.Windows.Forms.ToolStripButton forwardButton;
+        private System.Windows.Forms.ToolStripButton refreshButton;
+        private System.Windows.Forms.ToolStripButton homeButton;
+        private System.Windows.Forms.ToolStripTextBox searchBox;
+        private System.Windows.Forms.ToolStripButton searchButton;
+        private System.Windows.Forms.ToolStripButton addButton;
+        private System.Windows.Forms.ToolStripButton deleteButton;
+        private System.Windows.Forms.TabControl tabController;
+    }
+}
+
---
+++ Browser/MyBrowser.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Browser
+{
+    public partial class MyBrowser : Form
+    {
+        int tabCounter = 0;
+        public MyBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            WebBrowser browser = new WebBrowser();
+            browser.Visible = true;
+            browser.ScriptErrorsSuppressed = true;
+            browser.Dock = DockStyle.Fill;
+            browser.DocumentCompleted += Browser_DocumentCompleted;
+            tabController.TabPages.Add("Новая вкладка");
+            tabController.SelectTab(tabCounter);
+            tabController.SelectedTab.Controls.Add(browser);
+            tabCounter += 1;
+        }
+
+        private void Browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            tabController.SelectedTab.Text = ((WebBrowser)tabController.SelectedTab.Controls[0]).DocumentTitle;
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            if (searchBox.Text != null)
+            {
+                ((WebBrowser)tabController.SelectedTab.Controls[0]).Navigate(searchBox.Text);
+            }
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabController.SelectedTab.Controls[0]).GoBack();
+        }
+
+        private void ForwardButton_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabController.SelectedTab.Controls[0]).GoForward();
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabController.SelectedTab.Controls[0]).Refresh();
+        }
+
+        private void HomeButton_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabController.SelectedTab.Controls[0]).GoHome();
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (tabController.TabPages.Count > 1)
+            {
+                tabController.TabPages.RemoveAt(tabController.SelectedIndex);
+                tabController.SelectTab(tabController.TabPages.Count - 1);
+                tabCounter -= 1;
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void SearchBox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                ((WebBrowser)tabController.SelectedTab.Controls[0]).Navigate(searchBox.Text);
+            }
+        }
+
+        private void MyBrowser_Load(object sender, EventArgs e)
+        {
+            WebBrowser browser = new WebBrowser();
+            browser.Visible = true;
+            browser.ScriptErrorsSuppressed = true;
+            browser.Dock = DockStyle.Fill;
+            browser.DocumentCompleted += Browser_DocumentCompleted;
+            tabController.TabPages.Add("Новая вкладка");
+            tabController.SelectTab(tabCounter);
+            tabController.SelectedTab.Controls.Add(browser);
+            tabCounter += 1;
+        }
+    }
+}
